00345af9db7ed023a21ee47c69958f16f21ea209
zhanghaoran <2017929959@qq.com>
2022-06-06 20:48:16 +0800

    "zhr"

---
+++ controller/comment.go
@@ -0,0 +1,66 @@
+package controller
+
+import (
+	"douyinProject/entity"
+	"douyinProject/repository"
+	"douyinProject/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type commentreturn struct {
+	entity.Response
+	entity.User
+	Content   string `json:"content"`
+	CreatData string `json:"creat_data"`
+}
+type comment struct {
+	entity.Response
+	Id          int         `json:"id"`
+	User        entity.User `json:"user"`
+	Content     string      `json:"content"`
+	Create_data string      `json:"create_data"`
+}
+
+func Comment(c *gin.Context) {
+	token := c.Query("token")
+	video_id := c.Query("video_id")
+	action_type := c.Query("action_type")
+	if utils.TokenCheck(token) == false { // 验证token
+		c.JSON(http.StatusOK, entity.Response{
+			StatusCode: 1,
+			StatusMsg:  "error token",
+		})
+		return
+	}
+	id := utils.GetIdInToken(token)       // 拿到用户id
+	user, _ := repository.GetUserInfo(id) // 通过用户Id拿到用户结构体
+	id1, _ := strconv.Atoi(video_id)      // 获取视频id
+
+	if action_type == "1" { // 评论操作
+		com := entity.Comment{
+			Id:         id,
+			User:       user,
+			Content:    c.Query("Content"),
+			CreateDate: time.Now().String(),
+		}
+		var result repository.Com
+		result = repository.Uploadcomment(com, id1)
+
+		c.JSON(http.StatusOK, comment{
+			Response:    entity.Response{StatusCode: 1, StatusMsg: "success"},
+			Id:          int(result.Id),
+			User:        user,
+			Content:     result.Message,
+			Create_data: result.CreateData,
+		})
+	} else { // 删除评论
+		repository.DeleteComment(id1)
+		c.JSON(http.StatusOK, entity.Response{
+			StatusCode: 0,
+			StatusMsg:  "delete successfully",
+		})
+	}
+}
--- repository/datebase.go
+++ repository/datebase.go
@@ -1,13 +1,11 @@
 package repository
 
 import (
-	"log"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
-var dsn = "root:wang200315@tcp(localhost:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local"
+var dsn = "root:153359157aA@tcp(localhost:3306)/douyin?charset=utf8mb4&parseTime=True&loc=Local"
 var db *gorm.DB
 
 //type StoredUser struct {
@@ -23,15 +21,6 @@ func Init() {
 	}
 	db = database
 	//自动迁移，保证数据库是最新的
-	if err := db.AutoMigrate(&DbUser{}); err != nil {
-		log.Fatal(err)
-	}
-	if err := db.AutoMigrate(&dbVideo{}); err != nil {
-		log.Fatal(err)
-	}
-	if err := db.AutoMigrate(&UserFavorite{}); err != nil {
-		log.Fatal(err)
-	}
 }
 
 //测试用, 后续应该更改方案
